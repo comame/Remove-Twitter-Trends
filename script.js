39d38a46a93e8ded7e0be6d8a22964a42e61d1c2
comame <dev@comame.xyz>
2021-04-10 22:15:16 +0900

    あ

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const f = () => {
     const trends = document.querySelector('*[aria-label="タイムライン: トレンド"]')
     const trendsFeed = document.querySelector('*[aria-label="タイムライン: 話題を検索"]')
-    if (trends) trends.parentElement.removeChild(trends)
-    if (trendsFeed) trendsFeed.parentElement.removeChild(trendsFeed)
+
+    trends?.parentElement.removeChild(trends)
+    trendsFeed?.parentElement.removeChild(trendsFeed)
+
     requestAnimationFrame(f)
 }
 
