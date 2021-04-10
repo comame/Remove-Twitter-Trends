99833c829d97d6c41bd69cb07d23b72e2f242751
comame <dev@comame.xyz>
2021-04-10 22:05:41 +0900

    さらに強力にトレンドを隠す

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const f = () => {
     const trends = document.querySelector('*[aria-label="タイムライン: トレンド"]')
+    const trendsFeed = document.querySelector('*[aria-label="タイムライン: 話題を検索"]')
     if (trends) trends.parentElement.removeChild(trends)
+    if (trendsFeed) trendsFeed.parentElement.removeChild(trendsFeed)
     requestAnimationFrame(f)
 }
 
