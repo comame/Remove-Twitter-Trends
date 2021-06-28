c403886ba60c4ee39a952e2c2b1f5dd951e8164a
comame <dev@comame.xyz>
2021-06-28 23:58:13 +0900

    サイドバーの検索ボタンも削除

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const f = () => {
     const trends = document.querySelector('*[aria-label="タイムライン: トレンド"]')
     const trendsFeed = document.querySelector('*[aria-label="タイムライン: 話題を検索"]')
+    const searchButton = document.querySelector('*[aria-label="調べたいものを検索"')
 
     trends?.parentElement.removeChild(trends)
     trendsFeed?.parentElement.removeChild(trendsFeed)
+    searchButton?.parentElement.removeChild(searchButton)
 
     requestAnimationFrame(f)
 }
